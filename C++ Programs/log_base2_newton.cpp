8a6f443d1cced06f6952d1b123e90ee942c0682e
Prashanth Kumar G <prashantkumarrrg777@outlook.com>
2025-09-24 00:43:12 +0530

    added log programs in c++

--- C++ Programs/log_base2_newton.cpp
+++ C++ Programs/log_base2_newton.cpp
@@ -20,36 +20,36 @@ double twoPower(double x, double eps = 1e-10)
     return sum;
 }
 
-double logBase10NetwonMethod(double n, double eps = 1e-10)
+double logBase2NewtonMethod(double n, double eps = 1e-10)
 {
-    if(n < 0) n = -n;
+    if(n < 0) n = -n;           // just for handling, find partial value, fully value find in main (complex value)
 
     if(n == 1) return 0.0;      // actual result
 
-    if(n == 0) return 0.0;      // just for continuity
+    if(n == 0) return 0.0;      // just for handling and time complexity, return quickly, managed in main, not acual value
 
-    int k = 0;          // log2(n) = log2(m.10^k) = log2(m) + k, where 1 <= m < 10
-    while(n >= 10)      // scale down if n is large
+    int k = 0;         // log₂(n) = log₂(m.2^k) = log₂(m) + k, where 1 <= m < 2
+    while(n >= 2)      // scale down if n is large
     {
-        n = n / 10;     // e.g., { n = 3101, k = 0 } -> { n = 310.1, k = 1 } -> { n = 31.01, k = 2 } -> { n = 3.101, k = 3} 
+        n = n / 2;     // e.g., { n = 11, k = 0 } -> { n = 5.5, k = 1 } -> { n = 2.75, k = 2 } -> { n = 1.375, k = 3} 
         k++;
     }
     while(n < 1)        // scale up if n is large
     {
-        n = n * 10;     // e.g., { n = 0.003101, k = 0 } -> { n = 0.03101, k = -1 } -> { n = 0.3101, k = -2 } -> { n = 3.101, k = -3}
+        n = n * 2;     // e.g., { n = 0.011, k = 0 } -> { n = 0.022, k = -1 } -> { n = 0.044, k = -2 } -> { n = 0.088, k = -3} -> { n = 0.176, k = -4 } -> { n = 0.352, k = -5 } -> { n = 0.704, k = -6 } -> { n = 1.408, k = -7 }
         k--;
     }
 
-    double x = n / 10.0;     // initial guess ( 1 <= n < 10, final answer = log(n) + k )
+    double x = n / 2.0;     // initial guess ( 1 <= n < 2, final answer = log₂(n) + k )
 
     while(1)
     {
-        double x_new = x + ((n / twoPower(x)) - 1) / LN2;   // newton-raphson method - X(k+1) = X(k) + ( (( n / 10^(X(k)) ) - 1) / LN2 )
+        double x_new = x + ((n / twoPower(x)) - 1) / LN2;   // newton-raphson method - X(k+1) = X(k) + ( (( n / 2^(X(k)) ) - 1) / LN2 )
 
         double diff = (x_new > x) ? (x_new - x) : (x - x_new);
 
         if(diff < eps)
-            return x_new + k;       // e.g., return log2(3.101) + k, where k = 3 for n = 3101 or k = -3 for n = 0.003101
+            return x_new + k;       // e.g., return log₂(1.375) + k, where k = 3 for n = 11 (or) return log₂(1.408) + k, where k = -7 for n = 0.011
 
         x = x_new;
     }
@@ -62,21 +62,21 @@ int main()
     cout<<"\n Enter a number : ";
     cin>>number;
 
-    double result = logBase10NetwonMethod(number);
+    double result = logBase2NewtonMethod(number);
     long long intPart = result;
 
     if(number < 0)
         if(result - intPart > 1e-10)
-            cout<<noshowpoint<<setprecision(11)<<" \n log2 of "<<number<<" = "<<fixed<<setprecision(10)<<result<<" + "<<(PI / LN2)<<"i\n"<<endl;
+            cout<<noshowpoint<<setprecision(11)<<"\n log₂ of "<<number<<" = "<<fixed<<setprecision(10)<<result<<" + "<<(PI / LN2)<<"i\n"<<endl;
         else
-            cout<<noshowpoint<<setprecision(11)<<" \n log2 of "<<number<<" = "<<intPart<<" + "<<(PI / LN2)<<"i\n"<<endl;
+            cout<<noshowpoint<<setprecision(11)<<"\n log₂ of "<<number<<" = "<<intPart<<" + "<<(PI / LN2)<<"i\n"<<endl;
     else if(number == 0)
-            cout<<" \n log2 of "<<number<<" = "<<u8"-∞"<<"\n"<<endl;
+            cout<<"\n log₂ of "<<number<<" = "<<"-∞"<<"\n"<<endl;
     else
         if(result - intPart > 1e-10)
-            cout<<noshowpoint<<setprecision(11)<<" \n log2 of "<<number<<" = "<<fixed<<setprecision(10)<<result<<"\n"<<endl;
+            cout<<noshowpoint<<setprecision(11)<<"\n log₂ of "<<number<<" = "<<fixed<<setprecision(10)<<result<<"\n"<<endl;
         else
-            cout<<noshowpoint<<setprecision(11)<<" \n log2 of "<<number<<" = "<<intPart<<"\n"<<endl;
+            cout<<noshowpoint<<setprecision(11)<<"\n log₂ of "<<number<<" = "<<intPart<<"\n"<<endl;
 
     return 0;
 }
---
+++ C++ Programs/log_baseE_newton.cpp
@@ -0,0 +1,82 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+const double E = 2.7182818285;
+const double PI = 3.1415926536;
+
+double expPower(double x, double eps = 1e-10)
+{
+    double term = 1.0, sum = 1.0;
+    int f = 1;
+
+    while(term > eps)               // e^x = 1 + x/1! + (x^2)/2! + (x^3)/3! + (x^4)/4! + ...
+    {
+        term = term * x / f;        // next term = previous term * y / f
+        sum = sum + term;
+        f++;
+    }
+
+    return sum;
+}
+
+double logBaseENewtonMethod(double n, double eps = 1e-10)
+{
+    if(n < 0) n = -n;           // just for handling, find partial value, fully value find in main (complex value)
+
+    if(n == 1) return 0.0;      // actual result
+
+    if(n == 0) return 0.0;      // just for handling and time complexity, return quickly, managed in main, not acual value
+
+    int k = 0;         // ln(n) = ln(m.E^k) = ln(m) + k, where 1 <= m < E
+    while(n >= E)      // scale down if n is large
+    {
+        n = n / E;     // e.g., { n = 11, k = 0 } -> { n = 4.4066.., k = 2 } -> { n = 1.4886.., k = 3 }
+        k++;
+    }
+    while(n < 1)        // scale up if n is large
+    {
+        n = n * E;     // e.g., { n = 0.011, k = 0 } -> { n = 0.0299.., k = -1 } -> { n = 0.0812.., k = -2 } -> { n = 0.2209.., k = -3} -> { n = 0.6005, k = -4 } -> { n = 1.6325.., k = -5 }
+        k--;
+    }
+
+    double x = n / E;     // initial guess ( 1 <= n < E, final answer = ln(n) + k )
+
+    while(1)
+    {
+        double x_new = x + ((n / expPower(x)) - 1);   // newton-raphson method - X(k+1) = X(k) + (( n / e^(X(k)) ) - 1)
+
+        double diff = (x_new > x) ? (x_new - x) : (x - x_new);
+
+        if(diff < eps)
+            return x_new + k;       // e.g., return ln(1.4886..) + k, where k = 3 for n = 11 or return ln(1.6325..) + k, where k = -5 for n = 0.011
+
+        x = x_new;
+    }
+
+} 
+
+int main()
+{
+    double number; 
+    cout<<"\n Enter a number : ";
+    cin>>number;
+
+    double result = logBaseENewtonMethod(number);
+    long long intPart = result;
+
+    if(number < 0)
+        if(result - intPart > 1e-10)
+            cout<<noshowpoint<<setprecision(11)<<"\n ln "<<number<<" = "<<fixed<<setprecision(10)<<result<<" + "<<PI<<"i\n"<<endl;
+        else
+            cout<<noshowpoint<<setprecision(11)<<"\n ln "<<number<<" = "<<intPart<<" + "<<PI<<"i\n"<<endl;
+    else if(number == 0)
+            cout<<"\n ln "<<number<<" = "<<"-∞"<<"\n"<<endl;
+    else
+        if(result - intPart > 1e-10)
+            cout<<noshowpoint<<setprecision(11)<<"\n ln "<<number<<" = "<<fixed<<setprecision(10)<<result<<"\n"<<endl;
+        else
+            cout<<noshowpoint<<setprecision(11)<<"\n ln "<<number<<" = "<<intPart<<"\n"<<endl;
+
+    return 0;
+}
