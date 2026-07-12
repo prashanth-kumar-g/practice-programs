2d92c6af4573933c53b480d0f4d2db39114ec86b
Prashanth Kumar G <prashantkumarrrg777@outlook.com>
2026-07-12 14:56:22 +0530

    updated

---
+++ Java Programs/Practice/IPAExam1_b.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class IPAExam1_b
+{
+	public static int CountWords(String str)
+	{
+		if (str == null || str.trim().isEmpty())
+		{
+			return 0;
+		}
+		
+		String[] words = str.trim().split("\\s+");
+		
+		return words.length;
+	}
+
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		String str = sc.nextLine();
+			
+		int result = CountWords(str);
+		
+		System.out.println(result);
+		
+		sc.close();
+	}
+}
